1e0b253c6e999431e8c940387da93ee4b5771084
langereissilvio <silvio.langereis@sensotec.be>
2017-04-06 11:44:59 +0200

    CRUD operations and search field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; //<--NgModel lives here
+import { FormsModule } from '@angular/forms'; // <--NgModel lives here
+import { HttpModule } from '@angular/http';
 
 import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent } from './dashboard.component';
@@ -10,13 +11,19 @@ import { HeroService } from './hero.service';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+import {HeroSearchComponent} from './hero-search.component';
+
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
 ], // <-- import relevant modules
-  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent, HeroSearchComponent ],
   providers: [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
